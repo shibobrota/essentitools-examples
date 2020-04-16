14a833705ce65dbe22fb04346ef7521d77c15de3
Shibobrota <das@divami.com>
2020-04-16 16:45:59 +0530

    Added publish config

--- app/src/main/java/com/shibobrota/essentitoolsexamples/MainActivity.java
+++ app/src/main/java/com/shibobrota/essentitoolsexamples/MainActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.shibobrota.essentitools.Log;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("UNIQUE_TAG", "A Long, Very Long message.");
     }
 }
